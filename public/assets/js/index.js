f202c843df805a29e116fce18310eeedd3535f88
mhans003 <michaeledwardhanson@gmail.com>
2020-12-09 15:19:00 -0800

    Started process of convering to PWA: Created functions that will be used to connect to IndexedDB when offline; Installed webpack and Babel; Congfigured webpack.config.js to create bundle of index.js as well as icons; Changed references in index.html to bundled versions of the code; Created reference to manifest file index.html; Included necessary scripts in package.json in order to make webpack function. Ran npm run build to create dist/ folder. Configured MongoDB Atlas.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -168,3 +168,75 @@ document.querySelector("#add-btn").onclick = function() {
 document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
 };
+
+//INDEXEDDB 
+
+//Ensure that IndexedDB is possible in the browser.
+function verifyIndexedDB() {
+  if (!window.indexedDB) {
+    console.log("Your browser doesn't support IndexedDB. There may be a problem saving/storing budget data.");
+    alert("Your browser doesn't support IndexedDB. There may be a problem saving/storing budget data.");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//This function interacts with IndexedDB to perform CRUD operations. 'Object' will be ommitted when getting from the DB.
+//The fourth argument 'object' will contain the properties of a new transaction when "putting" into the DB
+//The fourth argument 'object' will contain an ID when deleting. 
+function useIndexedDB(databaseName, storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    //Open IndexedDB using the DB name passed in.
+    const request = window.indexedDB.open(databaseName, 1);
+    let db,
+      tx,
+      store;
+
+    request.onupgradeneeded = function(e) {
+      const db = request.result;
+      //Create the name of the transactions store.
+      db.createObjectStore(storeName, { keyPath: "_id" });
+    };
+
+    request.onerror = function(e) {
+      //Log an error if needed.
+      console.log("There was an error");
+      alert("Something went wrong with IndexedDB.");
+    };
+
+    //If the request to open IndexedDB is successful:
+    request.onsuccess = function(e) {
+      //Identify the DB.
+      db = request.result;
+      //Create a transaction with the desired store.
+      tx = db.transaction(storeName, "readwrite");
+      store = tx.objectStore(storeName);
+
+      db.onerror = function(e) {
+        console.log("error");
+        alert("Something went wrong trying to access the correct store.");
+      };
+      //If the method passed in is 'put', store the passed in object.
+      if (method === "put") {
+        store.put(object);
+      } else if (method === "get") {
+        //If the method is 'get', grab all the items stored in indexedDB.
+        const all = store.getAll();
+        all.onsuccess = function() {
+          //Resolve the promise.
+          resolve(all.result);
+        };
+        //If the method is 'delete', delete the transaction with the ID passed iin.
+      } else if (method === "delete") {
+        store.delete(object._id);
+      }
+      tx.oncomplete = function() {
+        //Close the DB once operation is finished.
+        db.close();
+      };
+    };
+  });
+}
+
+
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+    entry: "./public/assets/js/index.js",
+    output: {
+      path: __dirname + "/public/dist",
+      filename: "index.bundle.js"
+    },
+    mode: "development",
+    plugins: [
+      new WebpackPwaManifest({
+        name: "Budget Tracker",
+        short_name: "Budget Tracker",
+        description: "An application for keeping track of budget transactions.",
+        background_color: "#01579b",
+        theme_color: "#ffffff",
+        "theme-color": "#ffffff",
+        start_url: "/",
+        icons: [
+          {
+            src: path.resolve("public/assets/icons/icon-192x192.png"),
+            sizes: [512],
+            destination: path.join("assets", "icons")
+          }
+        ]
+      })
+    ],
+    //Configure Babel
+    module: {
+      rules: [
+        {
+          test: /\.js$/, // files must end in ".js" to be transpiled
+          exclude: /node_modules/, // don't transpile code from "node_modules"
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        }
+      ]
+    }
+  };
+  
+  module.exports = config;
